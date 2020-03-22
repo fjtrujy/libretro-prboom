1001e944b0aa6de89d890f8644891fa9dd42e831
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2020-03-22 12:37:34 +0100

    tmp2

--- src/p_maputl.c
+++ src/p_maputl.c
@@ -114,8 +114,7 @@ static int P_PointOnDivlineSide(fixed_t x, fixed_t y, const divline_t *line)
     !line->dx ? x <= line->x ? line->dy > 0 : line->dy < 0 :
     !line->dy ? y <= line->y ? line->dx < 0 : line->dx > 0 :
     (line->dy^line->dx^(x -= line->x)^(y -= line->y)) < 0 ? (line->dy^x) < 0 :
-    // FixedMul(y>>8, line->dx>>8) >= FixedMul(line->dy>>8, x>>8);
-    ((y>>8) * (line->dx>>8)) >> FRACBITS >= ((line->dy>>8) * (x>>8)) >> FRACBITS;
+    FixedMul(y>>8, line->dx>>8) >= FixedMul(line->dy>>8, x>>8);
 }
 
 //
